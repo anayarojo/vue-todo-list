60188c07205a019c5a787bbdec0ab8077ee83b17
anayarojo <raulmartin_04@hotmail.com>
2020-04-12 19:22:47 -0700

    Refactory task components

---
+++ src/components/ComTaskForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="component-task-form">
+    <el-card class="box-card" :shadow="shadow">
+      <input v-model="description" v-on:keyup.enter="submit"
+        type="text" class="w-full outline-none" placeholder="¿Qué pendiente hay?"
+      />
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComTaskForm',
+  props: {
+    shadow: {
+      type: String,
+      default: 'always',
+    },
+  },
+  data() {
+    return {
+      description: '',
+    };
+  },
+  methods: {
+    submit: function() {
+      if (this.description.trim() !== '') {
+          this.$emit('add-task', this.description.trim());
+          this.input = '';  
+      }
+    },
+  },
+};
+</script>
--- src/components/ComTaskList.vue
+++ src/components/ComTaskList.vue
@@ -1,85 +1,38 @@
 <template>
   <div class="component-task-list" :class="className">
-    <com-form @add-new-task="addTask" />
-    <com-container>
-      <el-card v-if="tasks.length > 0" class="box-card" :body-style="{ padding: '20px 0px' }">
+    <el-card v-if="tasks.length > 0" class="box-card" :body-style="{ padding: '20px 0px' }">
         <com-task
-          v-for="(task, index) in tasks"
-          :key="task.uuid"
-          :index="index"
-          :task="task"
-          :length="tasks.length"
-          @update-task="updateTask"
-          @delete-task="deleteTask"
+          v-for="(task, index) in tasks" :key="task.id || task.uuid" 
+          :index="index" :task="task" :length="tasks.length"
+          @update-task="updateTask" @delete-task="deleteTask"
         />
       </el-card>
-      <div class="text-center mt-3">
-        <p>¿Quieres guardar tus listas en la nube?</p>
-        <template v-if="!isLogged">
-          <p>
-            <span>
-              <el-link class="-mt-1 text-base" type="primary" @click="$router.push('register')">
-                Regístrate
-              </el-link>
-            </span>
-            o
-            <span>
-              <el-link class="-mt-1 text-base" type="primary" @click="$router.push('login')">
-                Inicia sesión
-              </el-link>
-            </span>
-          </p>
-        </template>
-        <template v-else>
-          <p>
-            <span>
-              <el-link class="-mt-1 text-base" type="primary" @click="$router.push('dashboard')">
-                Ir al dashboard
-              </el-link>
-            </span>
-          </p>
-        </template>
-      </div>
-    </com-container>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions, mapState } from 'vuex';
-import ComForm from '@/components/ComForm';
-import ComTask from '@/components/ComTask';
-import ComContainer from '@/components/ComContainer';
+import ComTask from '@/components/ComTaskItem';
 
 export default {
   name: 'ComTaskList',
   components: {
-    ComContainer,
-    ComForm,
     ComTask,
   },
   props: {
+    tasks: {
+      type: Array,
+    },
     className: {
       type: String,
     },
   },
-  computed: {
-    ...mapGetters('session', [
-      'isLogged',
-    ]),
-    ...mapGetters('localTasks', [
-      'completedTasks',
-      'pendingTasks',
-    ]),
-    ...mapState({
-      tasks: state => state.localTasks.all 
-    }),
-  },
   methods: {
-    ...mapActions('localTasks', [
-      'addTask',
-      'updateTask',
-      'deleteTask',
-    ]),
+    updateTask(task) {
+      return this.$emit('update-task', task);
+    },
+    deleteTask(task) {
+      return this.$emit('delete-task', task);
+    }
   }
 };
 </script>
