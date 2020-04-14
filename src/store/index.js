674b019fb64603f8877f1daf4f815e87252961dc
anayarojo <raulmartin_04@hotmail.com>
2020-04-13 22:08:04 -0700

    Remove unnecessary store modules

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import session from '@/store/modules/session';
 import localTasks from '@/store/modules/localTasks';
-import categories from '@/store/modules/categories';
-import lists from '@/store/modules/lists';
 import tasks from '@/store/modules/tasks';
 import plugins from '@/store/plugins';
 
@@ -13,8 +11,6 @@ export default new Vuex.Store({
   modules: {
     session,
     localTasks,
-    categories,
-    lists,
     tasks,
   },
   plugins,
